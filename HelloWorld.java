298dc5c2561b3d24aaa6a4667c22d07ab6f60fa5
LuisBanderasS <131292501+LuisBanderasS@users.noreply.github.com>
2023-04-21 09:50:47 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("-----***** Triyng Webhoo ******------");
+    System.out.println("-----***** Triyng Webhook ******------");
   }
 }
